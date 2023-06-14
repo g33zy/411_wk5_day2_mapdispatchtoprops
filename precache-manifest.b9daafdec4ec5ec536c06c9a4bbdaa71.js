432d187b025a0f2780b7166120d7591cd70b7758
g33zy <stevejgaz@gmail.com>
2023-06-14 17:24:37 -0500

    Updates

---
+++ precache-manifest.b9daafdec4ec5ec536c06c9a4bbdaa71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfc19bc35da71fac2adb1c4867f71ba1",
+    "url": "/411_wk5_day2_mapdispatchtoprops/index.html"
+  },
+  {
+    "revision": "2087badc56e6203724f8",
+    "url": "/411_wk5_day2_mapdispatchtoprops/static/css/main.be9515fe.chunk.css"
+  },
+  {
+    "revision": "c80f923e0f6381083430",
+    "url": "/411_wk5_day2_mapdispatchtoprops/static/js/2.742afeec.chunk.js"
+  },
+  {
+    "revision": "2087badc56e6203724f8",
+    "url": "/411_wk5_day2_mapdispatchtoprops/static/js/main.21a284e1.chunk.js"
+  },
+  {
+    "revision": "e47b0be5e9874947a62d",
+    "url": "/411_wk5_day2_mapdispatchtoprops/static/js/runtime~main.df0822c9.js"
+  }
+]);
